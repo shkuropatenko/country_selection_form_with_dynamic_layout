87da4c38bbd86bf5b96bf71957078eda56539281
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2024-11-27 16:18:15 -0500

    Added HTML layout and CSS styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./styles/Form.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    country: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Phone Number:
+        <input
+          type="tel"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Country:
+        <select
+          name="country"
+          value={formData.country}
+          onChange={handleChange}
+          required
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="">Select your country</option>
+          {countries.map((country) => (
+            <option key={country.cca2} value={country.name.common}>
+              {country.name.common}
+            </option>
+          ))}
+        </select>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
